5fb3dbb3fe986a8e60608f67c337a972fe2b27f3
TylerJStark <tyler4276@gmail.com>
2019-01-07 04:45:21 -0500

    Updates

---
+++ precache-manifest.4f1ef9cdcbc1796264d0df75d6ffd799.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "65012e6e8e784d3901aa",
+    "url": "/clicky-game/static/js/main.65012e6e.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clicky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "65012e6e8e784d3901aa",
+    "url": "/clicky-game/static/css/main.b59537dc.chunk.css"
+  },
+  {
+    "revision": "6716f5e90b287695bc7898d6805e9332",
+    "url": "/clicky-game/index.html"
+  }
+];
